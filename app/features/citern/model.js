12998b39ebb4dfcb0316900a4bf49e46bfbef0db
Unknown <Louis.gautier@outlook.fr>
2018-02-14 14:48:40 +0100

    add maxCapacity property in Citern Object

--- app/features/citern/model.js
+++ app/features/citern/model.js
@@ -12,7 +12,8 @@
 // const Citern = mongoose.model('citern', citernSchema)
 
 class Citern {
-    constructor (isGlobal, consumtion, id) {
+    constructor (isGlobal, consumtion, id, maxCapactity) {
+        this.maxCapacity = maxCapactity // the global citern as a maxCapacity greater than non global citern
         this.consumtion = consumtion
         this.id = id
         this.isGlobal = isGlobal
