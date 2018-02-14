4296e6b7e8e60103084fe165841f86e2f813d128
Doshibu <guillaume.bonhommeau@viacesi.fr>
2018-02-14 15:53:46 +0100

    Add brunch (still inactive)

--- server.js
+++ server.js
@@ -5,15 +5,32 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const dbURI = 'mongodb://louis:P@ssword1@ds233748.mlab.com:33748/dry-field'
 const app = express()
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose default connection open to ' + dbURI)
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err)
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected')
+})
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination')
+    process.exit(0)
+  })
+})
+mongoose.connect(dbURI)
 
 app.set('port', process.env.PORT || 3000)
+app.set('view engine', 'twig')
+app.set('views', path.join(__dirname, '/app/views'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.set('view engine', 'twig')
-
-app.set('views', path.join(__dirname, '/app/views'))
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/app', express.static(path.join(__dirname, 'app')))
 
@@ -21,26 +38,6 @@ app.get('/', (req, res) => {
   res.render('index.twig')
 })
 
-let server = app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + server.address().port)
-    mongoose.connect(dbURI, {useMongoClient: true})
-})
-
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose default connection open to ' + dbURI)
-})
-
-mongoose.connection.on('error', (err) => {
-    console.log('Mongoose default connection error: ' + err)
-})
-
-mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose default connection disconnected')
-})
-
-process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-        console.log('Mongoose default connection disconnected through app termination')
-        process.exit(0)
-    })
+const server = app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + server.address().port)
 })
