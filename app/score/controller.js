58ee855b1e3ba69f412b09281a4f093911731271
Unknown <Louis.gautier@outlook.fr>
2018-02-16 09:46:04 +0100

    fix test (one again)  downgrade mocha to 3.0.0

--- app/score/controller.js
+++ app/score/controller.js
@@ -10,14 +10,11 @@ module.exports = {
         })
     },
     saveScore: (req, res) => {
-        console.log(req.name)
         const toSave = new Score.ScoreModel(req)
-        console.log(toSave.playerName)
         toSave.save((err, result) => {
             if (err) {
                 res.status(202).send(err)
             }
-            console.log(result)
             res.status(201).send(result)
         })
     }
